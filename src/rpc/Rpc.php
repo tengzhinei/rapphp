e363925f4ac70b8d4c274d7d84c1a9c54c00b329
藤之内 <tengzhinei@promote.cache-dns.local>
2019-06-02 16:42:58 +0800

    rpc支持代码注册

--- src/rpc/Rpc.php
+++ src/rpc/Rpc.php
@@ -21,7 +21,9 @@ use rap\rpc\service\RpcHandlerMapping;
 use rap\ServerEvent;
 use rap\swoole\pool\Pool;
 use rap\swoole\pool\ResourcePool;
+use rap\web\mvc\AutoFindHandlerMapping;
 use rap\web\mvc\Dispatcher;
+use rap\web\mvc\Router;
 
 class Rpc {
 
@@ -38,7 +40,7 @@ class Rpc {
         $dispatcher = Ioc::get(Dispatcher::class);
         //rpc 提供方
         $config = Config::getFileConfig()[ 'rpc_service' ];
-        if ($config&&$config['open']) {
+        if ($config && $config[ 'open' ]) {
             $mapping = Ioc::get(RpcHandlerMapping::class);
             $dispatcher->addHandlerMapping($mapping);
         }
@@ -51,42 +53,65 @@ class Rpc {
         }
         foreach ($rpcs as $rpc => $config) {
             $register = $config[ 'register' ];
-            $client = $config[ 'client' ];
-            if (!$client) {
-                $client = IS_SWOOLE ? 'http2' : 'http';
-            }
             /* @var $register RpcRegister */
             $register = new $register;
-            $items = $register->register();
-            foreach ($items as $clazz => $downgrade) {
-                if (is_numeric($clazz)) {
-                    $clazz = $downgrade;
-                } else {
-                    Ioc::bind($clazz, $downgrade);
-                }
-                $this->rpc_inter[ $clazz ] = $rpc;
-                AopBuild::before($clazz)->methodsAll()->wave(RpcWave::class)->using('before')->addPoint();
-            }
-            if ($client == 'http2') {
-                Ioc::bind($rpc, RpcHttp2Client::class, function(RpcHttp2Client $client) use ($config) {
-                    $client->config($config);
-                });
-            } else {
-                Ioc::bind($rpc, RpcHttpClient::class, function(RpcHttpClient $client) use ($config) {
-                    $client->config($config);
-                });
-            }
-
-
-            //需要在 worker 进程调用
+            $this->initRpcClient($rpc,$register,$config);
         }
+
+    }
+
+
+    private function initRpcClient($name, RpcRegister $register, $config) {
+        $client = $config[ 'client' ];
+        if (!$client) {
+            $client = 'http';
+        }
+        /* @var $register RpcRegister */
+        $register = new $register;
+        $items = $register->register();
+        foreach ($items as $clazz => $downgrade) {
+            if (is_numeric($clazz)) {
+                $clazz = $downgrade;
+            } else {
+                Ioc::bind($clazz, $downgrade);
+            }
+            $this->rpc_inter[ $clazz ] = $name;
+            AopBuild::before($clazz)->methodsAll()->wave(RpcWave::class)->using('before')->addPoint();
+        }
+        if ($client == 'http2') {
+            Ioc::bind($name, RpcHttp2Client::class, function(RpcHttp2Client $client) use ($config) {
+                $client->config($config);
+            });
+        } else {
+            Ioc::bind($name, RpcHttpClient::class, function(RpcHttpClient $client) use ($config) {
+                $client->config($config);
+            });
+        }
+    }
+
+    public static function registerRpc($rpcName, $registerClazz) {
+        $rpcName=$rpcName.'__rpc';
+        Event::add(ServerEvent::onAppInit, function(AutoFindHandlerMapping $autoMapping, Router $router) use ($rpcName, $registerClazz) {
+            /* @var $rpc Rpc */
+            $rpc = Ioc::get(Rpc::class);
+            //    FileUtil::
+
+            $config = self::loadConfig($rpcName);
+            $rpc->initRpcClient($rpcName, $registerClazz, $config);
+
+        });
+        Event::add(ServerEvent::onServerWorkStart, function() use ($rpcName) {
+            if (IS_SWOOLE) {
+                ResourcePool::instance()->preparePool($rpcName);
+            }
+        });
 
     }
 
 
     public function onServerWorkStart() {
         $rpcs = Config::getFileConfig()[ 'rpc' ];
-        if(IS_SWOOLE){
+        if (IS_SWOOLE) {
             //注册连接池
             foreach ($rpcs as $rpc => $config) {
                 ResourcePool::instance()->preparePool($rpc);
@@ -104,5 +129,15 @@ class Rpc {
         $name = $this->rpc_inter[ $clazz ];
         return Pool::get($name);
     }
+
+    public static function loadConfig($name) {
+        $file = APP_PATH . 'config/rpc-config.php';
+        if (file_exists($file)) {
+            $data = include_once $file;
+            return $data[ $name ];
+        }
+        return [];
+    }
+
 
 }
